0e29384d9eac8b5b8e11e650256c9fba9a5d2006
Sudipta Mohanta <sudiptamohanta7479@gmail.com>
2025-08-16 15:51:12 +0530

    Add files via upload

---
+++ Appointment.java
@@ -0,0 +1,16 @@
+public class Appointment {
+    private Patient patient;
+    private Doctor doctor;
+    private String date;
+
+    public Appointment(Patient patient, Doctor doctor, String date) {
+        this.patient = patient;
+        this.doctor = doctor;
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment: [Patient: " + patient + ", Doctor: " + doctor + ", Date: " + date + "]";
+    }
+}
